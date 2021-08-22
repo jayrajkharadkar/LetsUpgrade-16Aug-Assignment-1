c46d6aca127ec95934da4b4eb4bcccd92aceda08
jayrajkharadkar <83096140+jayrajkharadkar@users.noreply.github.com>
2021-08-22 17:14:31 +0530

    Add files via upload

---
+++ src/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <div>
+            <h1>LetsUpgarde helped me to build this project</h1>
+        </div>
+    )
+}
+
+export default Banner;
