1ee3666795ad6a88c4b6c8d07e9cfaa8158075aa
jayrajkharadkar <83096140+jayrajkharadkar@users.noreply.github.com>
2021-08-22 14:39:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Banner from './Banner';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Banner />
+    </div>
+  );
+}
+
+export default App;
